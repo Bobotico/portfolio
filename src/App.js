49cae5261e58062b888764f3bb277ea0a36a5dff
Bobotico <francesco.borrelli.pro@gmail.com>
2022-09-16 12:53:49 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to my react portfolio!</h1>
+      <h2>Author: Francesco Borrelli</h2>
+    </div>
+  );
+}
+
+export default App;
