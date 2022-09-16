7a64f9c003f334e521a0449c8af5d4ff3e737dee
Bobotico <francesco.borrelli.pro@gmail.com>
2022-09-16 13:45:06 +0200

    third

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import logoCircle from '../cirlce_me.png';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to my react portfolio!</h1>
       <h2>Author: Francesco Borrelli</h2>
+      <img src="/circle_me.png" alt="circle layer"></img>
     </div>
   );
 }
